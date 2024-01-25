b3f843b038a79ada13214794ba1c1d203d9c78eb
Israel Akintunde <israelakintunde005@gmail.com>
2024-01-25 04:12:41 +0100

    chore: updated truncatetext functino and tests

--- src/truncateText/index.test.ts
+++ src/truncateText/index.test.ts
@@ -1,7 +1,7 @@
 import {truncateText} from '../index';
 
 describe('truncate text', () => {
-  it('should truncate text', () => {
-    expect(truncateText('test', 2)).toEqual('te...');
-  });
+  it('should truncate a text', () => {
+    expect(truncateText('foo bar baz', 3, 3)).toEqual('foo...baz');
+  })
 });
--- src/truncateText/index.ts
+++ src/truncateText/index.ts
@@ -2,11 +2,18 @@
  * Truncates a given text to a specified length, adding ellipsis if necessary.
  *
  * @param {string} text - The text to be truncated.
- * @param {number} length - The maximum length of the truncated text. Default value is 30.
+ * @param {number} limit - The maximum length of the truncated text. Default value is 30.
+ * @param {number} max - The maximum length of the truncated text. Default value is 30.
  * @return {string} The truncated text.
  */
-const truncateText = (text: string, length: number = 30): string =>
-  // @ts-ignore
-  text.length > length ? `${text.substring(0, length)}...` : text;
+const truncateText = (text: string, limit: number = 12, max: number = 18): string => {
+  if (text.length <= max) {
+    return text;
+  }
 
+  const first_string = text.substring(0, limit);
+  const second_string = text.substring(text.length - 3, text.length + 1);
+
+  return `${first_string}...${second_string}`;
+};
 export default truncateText;
